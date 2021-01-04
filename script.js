a0432c6873714270b1f37f1d5d60deda7d41ec61
rishr <shrivastva.r@husky.neu.edu>
2021-01-03 22:08:40 -0500

    Adding leaflet map location

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position);
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude];
+
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     }, function () {
         alert('could not get your position')
     });
